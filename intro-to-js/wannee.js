0c5ea3d70409e62137b0c034b7b232e6b1b5641e
wannee buason <wannee.bu19@gmail.com>
2022-02-18 14:34:47 +0700

    add calage and profile

--- intro-to-js/wannee.js
+++ intro-to-js/wannee.js
@@ -10,3 +10,21 @@ function helloWorld() {
     const h1message = document.getElementById("askMyName").innerHTML // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
     alert(h1message + 'ถ้ารีบแซงไปก่อนเลย')
 }
+
+function calAge() {
+    const yearOfBirth = Number(prompt("Please enter your year of birth in A.D"));
+
+    if (isNaN(yearOfBirth) || (yearOfBirth == 0) || (yearOfBirth == 1)) {
+        calAge();
+    } else {
+        const age = 2022 - yearOfBirth;
+        alert(`ยินดีด้วย คุณอยู่ในประเทศนี้มา ${age} ปีแล้ว`);
+    }
+}
+
+function loadProfile() {
+    const name = "Wannee Buason"
+    const province = "Bangkok"
+    document.getElementById("my-name").innerHTML = name;
+    document.getElementById("my-province").innerHTML = province;
+}
